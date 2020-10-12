27d9897a7d8fc531495afb323a6f815dc0189777
Baraka-Mugisha <mugishaje@gmail.com>
2020-10-12 10:35:41 +0200

    zippy exercises

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css']
 })
-export class ZippyComponent implements OnInit {
+export class ZippyComponent {
+  @Input('title') title: string;
 
-  constructor() { }
+  isExpanded: boolean = false;
 
-  ngOnInit(): void {
+  toggle(){
+    this.isExpanded = !this.isExpanded
   }
-
 }
