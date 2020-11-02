f550bd8171775407eb46d0c2a1c475f9aa3f1bab
Baraka-Mugisha <mugishaje@gmail.com>
2020-11-02 12:16:48 +0200

    update, delete and onInit

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
 @Component({
@@ -6,15 +6,10 @@ import { HttpClient } from '@angular/common/http'
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
-  posts: Object[];
+export class PostsComponent implements OnInit {
+  posts: any[];
   private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: HttpClient) {
-    http.get(this.url)
-      .subscribe(response => {
-        console.log('getting response... ', response);
-        this.posts = (response as Array<null>);
-      })
     }
     createPost(input: HTMLInputElement){
       let post = { title: input.value };
@@ -25,6 +20,28 @@ export class PostsComponent {
           post['id']=response['id'];
           console.log('creating response... ');
           this.posts.splice(0, 0, post);
+      })
+    }
+    updatePost(post) {
+      this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead : true }))
+        .subscribe(response => {
+          console.log(response)
+        })
+      // this.http.put(this.url, JSON.stringify(post))
+    }
+    deletePost(post) {
+      this.http.delete(this.url + '/' + post.id)
+        .subscribe(response => {
+          let id = this.posts.indexOf(post);
+          this.posts.splice(id, 1)
+          console.log(response)
+        })
+    }
+    ngOnInit(){
+      this.http.get(this.url)
+      .subscribe(response => {
+        console.log('getting response... ', response);
+        this.posts = (response as Array <any>);
       })
     }
 }
