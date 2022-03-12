79eda5d5c57247306c1487faa3a276a3262336e2
Baraka-Mugisha <mugishaje@gmail.com>
2022-03-12 23:50:55 +0200

    3 - Getting Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { CoursesComponent } from './courses/courses.component';
 import { CourseComponent } from './course.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule, EventEmitter } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -13,7 +13,6 @@ import { MovieSearchComponent } from './movie-search/movie-search.component';
 import { PrepositionPipe } from './preposition.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
-
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -27,10 +26,37 @@ import { CreateCourseComponent } from './create-course/create-course.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseComponent } from './new-course/new-course.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent, CoursesComponent, SummaryPipe, FavoriteComponent, MovieSearchComponent, PrepositionPipe, PanelComponent, LikeComponent, InputFormatDirective, ZippyComponent, ContactFormComponent, CreateCourseComponent, SignupFormComponent, NewCourseComponent, ChangePasswordComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule, FlexLayoutModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+    SummaryPipe,
+    FavoriteComponent,
+    MovieSearchComponent,
+    PrepositionPipe,
+    PanelComponent,
+    LikeComponent,
+    InputFormatDirective,
+    ZippyComponent,
+    ContactFormComponent,
+    CreateCourseComponent,
+    SignupFormComponent,
+    NewCourseComponent,
+    ChangePasswordComponent,
+    PostsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [CoursesService],
   bootstrap: [AppComponent],
 })
