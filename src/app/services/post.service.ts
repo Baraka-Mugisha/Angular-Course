729face10bc8ca94b6973f175c30540a9a0db280
Baraka-Mugisha <mugishaje@gmail.com>
2022-03-14 21:34:17 +0200

    17 - Extracting a reusable error handling method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -6,7 +6,7 @@ import { AppError } from '../common/app-error';
 import { NotFoundError } from '../common/not-found-error';
 import { BadRequestError } from '../common/bad-request-error';
 
-import 'rxjs/add/observable/throw'
+import 'rxjs/add/observable/throw';
 
 @Injectable({
   providedIn: 'root',
@@ -16,35 +16,33 @@ export class PostService {
 
   constructor(private http: HttpClient) {}
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url).pipe(catchError(this.handleError));
   }
 
   createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post)).pipe(
-      catchError((error: Response) => {
-        if (error.status === 400) {
-          throwError(new BadRequestError(error));
-        }
-        return throwError(new AppError(error));
-      })
-    );
+    return this.http
+      .post(this.url, JSON.stringify(post))
+      .pipe(catchError(this.handleError));
   }
 
   updatePost(id, payload) {
-    return this.http.patch(this.url + '/' + id, JSON.stringify(payload));
+    return this.http.patch(this.url + '/' + id, JSON.stringify(payload))
+    .pipe(catchError(this.handleError));
   }
 
   deletePost(id) {
-    return this.http.delete(this.url + '/' + id).pipe(
-      catchError((error: Response) => {
-        if (error.status === 404) {
-          throwError(new NotFoundError());
-        }
-        if (error.status === 400) {
-          throwError(new BadRequestError());
-        }
-        return throwError(new AppError(error));
-      })
-    );
+    return this.http
+      .delete(this.url + '/' + id)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: Response) {
+    if (error.status === 404) {
+      throwError(new NotFoundError());
+    }
+    if (error.status === 400) {
+      throwError(new BadRequestError());
+    }
+    return throwError(new AppError(error));
   }
 }
